f5eb47b6c72d8a3ee6ba09f682e9f250a712f0e2
Edimar Manica <manicaedimar@gmail.com>
2016-08-30 16:56:44 -0300

    Adicionando a possibilidade de carregar os resultados para o Postgres

---
+++ Metrics/src/main/java/br/edimarmanica/metrics/CSV2BD.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edimarmanica.metrics;
+
+import br.edimarmanica.configuration.Paths;
+import br.edimarmanica.dataset.Dataset;
+import br.edimarmanica.dataset.Domain;
+import br.edimarmanica.dataset.Site;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+/**
+ *
+ * @author edimar
+ */
+public class CSV2BD {
+
+    private final Site site;
+    private final String method;
+    private final String path;
+
+    public CSV2BD(Site site, String method, String path) {
+        this.site = site;
+        this.method = method;
+        this.path = path;
+    }
+
+    /**
+     * Excluir os resultados do método para o site no banco de dados
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    private void deleteSiteResults() throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM " + method + " WHERE base=? and dominio=? and site = ? ";
+        PreparedStatement ps = Conexao.getConexao().prepareStatement(sql);
+        ps.setString(1, site.getDomain().getDataset().toString());
+        ps.setString(2, site.getDomain().toString());
+        ps.setString(3, site.toString());
+        ps.execute();
+    }
+
+    private void insertSiteResults(String atributo, String regra, String rotulo, int relevantes, int recuperados, int relevantes_recuperados, double revocacao, double precisao, String data) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO " + method + " (base, dominio, site, atributo, regra, rotulo, relevantes, recuperados, relevantes_recuperados, revocacao, precisao, data_avaliacao) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement ps = Conexao.getConexao().prepareStatement(sql);
+        int i = 1;
+        ps.setString(i++, site.getDomain().getDataset().toString());
+        ps.setString(i++, site.getDomain().toString());
+        ps.setString(i++, site.toString());
+        ps.setString(i++, atributo);
+        ps.setString(i++, regra);
+        ps.setString(i++, rotulo);
+        ps.setInt(i++, relevantes);
+        ps.setInt(i++, recuperados);
+        ps.setInt(i++, relevantes_recuperados);
+        ps.setDouble(i++, revocacao);
+        ps.setDouble(i++, precisao);
+        ps.setString(i++, data);
+        ps.execute();
+    }
+
+    private void execute() throws SQLException, ClassNotFoundException {
+        Conexao.getConexao().setAutoCommit(false);
+        deleteSiteResults();
+
+        try (Reader in = new FileReader(path + "/" + site.getPath() + "/result.csv")) {
+            try (CSVParser parser = new CSVParser(in, CSVFormat.EXCEL.withHeader())) {
+
+                for (CSVRecord record : parser) {
+                    insertSiteResults(record.get("ATTRIBUTE"), record.get("RULE"), record.get("LABEL"), Integer.parseInt(record.get("RELEVANTS")), Integer.parseInt(record.get("RETRIEVED")), Integer.parseInt(record.get("RETRIEVED RELEVANTS")), Double.parseDouble(record.get("RECALL")), Double.parseDouble(record.get("PRECISION")), record.get("DATE"));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MergeResults.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MergeResults.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Conexao.getConexao().commit();
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> methodPath = new HashMap<>();
+        methodPath.put("identificacaomanual", Paths.PATH_TEMPLATE_VARIATION_MANUAL);
+        methodPath.put("weir", Paths.PATH_INTRASITE);
+        methodPath.put("trinity", Paths.PATH_TRINITY+"/ved_w1/");
+        methodPath.put("orion", Paths.PATH_TEMPLATE_VARIATION_AUTO);
+        
+        String method = "orion";
+
+        for (Dataset dataset : Dataset.values()) {
+            for (Domain domain : dataset.getDomains()) {
+                for (Site site : domain.getSites()) {
+                    CSV2BD ex = new CSV2BD(site, method, methodPath.get(method));
+                    try {
+                        ex.execute();
+                    } catch (SQLException | ClassNotFoundException ex1) {
+                        Logger.getLogger(CSV2BD.class.getName()).log(Level.SEVERE, null, ex1);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Metrics/src/main/java/br/edimarmanica/metrics/Conexao.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edimarmanica.metrics;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author edimar
+ */
+public class Conexao {
+    private static Connection conn;
+    private final static String driver = "org.postgresql.Driver";
+    private final static String ip = "localhost/";
+    public static String dataBase = "extraction_results";
+    public static String user = "edimar";
+    public static String password = "edimar";
+ 
+    public Conexao(Connection conn) {
+        this.conn = conn;
+    }
+ 
+    public static Connection getConexao() throws SQLException, ClassNotFoundException {
+ 
+        if (conn != null) {
+            return conn;
+        }
+ 
+        Class.forName(driver);
+        conn = java.sql.DriverManager.getConnection("jdbc:postgresql://" + ip + dataBase, user, password);
+        return conn;
+ 
+    }
+ 
+    public static void fechaConexao() {
+        try {
+            conn.close();
+            conn = null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
